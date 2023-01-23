a9c4f62005672142b86a2da0b4205107cc37bf00
akakiplimo <akakiplimo@gmail.com>
2023-01-23 13:53:07 -0800

    task: 2. Adding CSS & Images

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"] 
+      },
+      {  
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      use: [
+          'file-loader',
+	  {
+	    loader: 'image-webpack-loader',
+	    options: {
+	      bypassOnDebug: true,
+	      disable: true,
+	    },
+	  },
+      ],
+    ]
+  }
+};
