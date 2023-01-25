13e4d0160c87fa3047faa3ad66715cb759de33e1
Adrian <adrian@novek.io>
2023-01-25 21:33:01 +0300

    Updates

---
+++ precache-manifest.238a02dc35ce05867d3d48a66663ade9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db4d9912231bae866be6",
+    "url": "/alx-react/static/css/main.2f6afc93.chunk.css"
+  },
+  {
+    "revision": "db4d9912231bae866be6",
+    "url": "/alx-react/static/js/main.db4d9912.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/alx-react/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "6c2a5e765b1392d8b38d",
+    "url": "/alx-react/static/js/2.6c2a5e76.chunk.js"
+  },
+  {
+    "revision": "898728b952bd5079aad1",
+    "url": "/alx-react/static/js/runtime~main.898728b9.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/alx-react/static/media/holberton-logo.23d20eb3.jpg"
+  },
+  {
+    "revision": "787bf4eec6153eb15076fc66352ab123",
+    "url": "/alx-react/index.html"
+  }
+];
