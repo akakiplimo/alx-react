0d9d6a5f5827af8866c217f3055e38c0be9601b7
Adrian <adrian@novek.io>
2023-01-25 22:58:54 +0300

    task done: 9. Install Babel

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -8,6 +8,13 @@ module.exports = {
     // Path to your entry point. From this file Webpack will begin its work
     entry: './src/index.js',
 
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on the final bundle. For now, we don't need production's JavaScript 
+    // minifying and other things, so let's set mode to development
+    mode: 'development',
+    devtool: "inline-source-map",
+
     // Path and filename of your result bundle.
     // Webpack will bundle all JavaScript into this file
     output: {
@@ -15,10 +22,39 @@ module.exports = {
         publicPath: '',
         filename: 'bundle.js'
     },
-
-    // Default mode for Webpack is production.
-    // Depending on mode Webpack will apply different things
-    // on the final bundle. For now, we don't need production's JavaScript 
-    // minifying and other things, so let's set mode to development
-    mode: 'development'
-}
+    devServer: {
+        hot: true,
+        contentBase: path.resolve("./dist"),
+        compress: true,
+        port: 8564,
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
