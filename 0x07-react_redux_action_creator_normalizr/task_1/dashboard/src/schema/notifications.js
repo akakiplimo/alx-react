214fa8780247a3c74006ee12e46718b04c5ea7f4
abracodeabra <akakiplimo@gmail.com>
2023-02-24 14:34:13 +0300

    task: 1. Normalize a nested JSON

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -4,9 +4,30 @@
 // when the author id is the same as the userId
 
 import * as notificationItem from "../../notifications.json";
+import { normalize, schema } from "normalizr";
 
-export const getAllNotificationsByUser = (userId) => {
+export default getAllNotificationsByUser = (userId) => {
   return notificationItem.default
     .filter((item) => item.author.id === userId)
     .map(({ context }) => context);
 };
+
+// User schema defined
+const user = new schema.Entity("users");
+
+// Message schema defined
+const message = new schema.Entity(
+  "messages",
+  {},
+  {
+    idAttribute: "guid",
+  }
+);
+
+// Notification schema defined
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notificationItem, [notification]);
