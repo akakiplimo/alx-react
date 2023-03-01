ed1a527ece03340a118b38e7417c23c9bf7980f4
Adrian Kiplimo <adrian@novek.io>
2023-03-01 15:47:48 +0300

    fix: remove logs

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -16,8 +16,6 @@ describe("uiReducer", function () {
 
   it("when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property", () => {
     const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
-    console.log("log something: ", state);
-    console.log(state.toJS());
     expect(state.toJS()).toEqual({
       ...INITIAL_STATE,
       isNotificationDrawerVisible: true,
