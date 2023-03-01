7d00426e5ee13c1a0962e3527f8d3c0ae19b4a0b
Adrian Kiplimo <adrian@novek.io>
2023-03-01 14:29:54 +0300

    task: 1. Use Immutable for the UI Reducer

---
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,26 @@
+import { toJS } from "immutable";
+import uiReducer, { INITIAL_STATE } from "./uiReducer";
+import { SELECT_COURSE } from "../actions/courseActionTypes";
+import { DISPLAY_NOTIFICATION_DRAWER } from "../actions/uiActionTypes";
+
+describe("uiReducer", function () {
+  it("state returned equals the initial state when no action is passed", () => {
+    const state = uiReducer(undefined, {});
+    expect(state.toJS()).toEqual(INITIAL_STATE);
+  });
+
+  it("equals the initial state when the action SELECT_COURSE is passed", () => {
+    const state = uiReducer(undefined, { type: SELECT_COURSE });
+    expect(state.toJS()).toEqual(INITIAL_STATE);
+  });
+
+  it("when the action DISPLAY_NOTIFICATION_DRAWER is passed, changes correctly the isNotificationDrawerVisible property", () => {
+    const state = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
+    console.log("log something: ", state);
+    console.log(state.toJS());
+    expect(state.toJS()).toEqual({
+      ...INITIAL_STATE,
+      isNotificationDrawerVisible: true,
+    });
+  });
+});
