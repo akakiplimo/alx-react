76adcdecb3a0ff8c4c1d3a35f46d03c6458ba30b
abracodeabra <akakiplimo@gmail.com>
2023-03-02 21:17:51 +0300

    task: 3. Create the reducer for notifications

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/notificationReducer.js
@@ -0,0 +1,46 @@
+import {
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  FETCH_NOTIFICATIONS_SUCCESS,
+} from "../actions/notificationActionTypes";
+
+export const INITIAL_STATE = {
+  notifications: [],
+  filter: "",
+};
+
+const notificationReducter = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case FETCH_NOTIFICATIONS_SUCCESS:
+      return action.data.map((notification) => {
+        return {
+          ...notification,
+          isRead: true,
+        };
+      });
+
+    case MARK_AS_READ:
+      return {
+        ...state,
+        notifications: state.notifications.map((notification) => {
+          const current = {
+            ...notification,
+          };
+          if (notification.id == action.index) current.isRead = true;
+
+          return current;
+        }),
+      };
+
+    case SET_TYPE_FILTER:
+      return {
+        ...state,
+        filter: action.filter,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default notificationReducter;
