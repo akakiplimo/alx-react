21087dc1faa72e996b0153c148589f643d22f81b
abracodeabra <akakiplimo@gmail.com>
2023-03-07 08:24:22 +0300

    task: 1. Create a small store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,17 @@
+import { Map } from 'immutable';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './App/App';
+import uiReducer, { INITIAL_STATE } from './reducers/uiReducer';
+
+const store = createStore(uiReducer, Map(INITIAL_STATE))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
